04d4bbda42c72fbe1a059d47ac1ed86fbab22b0a
kawalec <kawalec@gmail.com>
2020-06-15 14:36:11 +0200

    Updates

---
+++ precache-manifest.22c53a2b1309afd20acf4113c1efa58a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b01ad840908d43ff23d541a6275941",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "9ffe2b83df34fdc6d0c6",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f507f1523e0ae6d5274b",
+    "url": "/react-gh-pages/static/js/2.09829594.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.09829594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ffe2b83df34fdc6d0c6",
+    "url": "/react-gh-pages/static/js/main.85f93bfb.chunk.js"
+  },
+  {
+    "revision": "10c5fbf93483f1318ac1",
+    "url": "/react-gh-pages/static/js/runtime-main.d4118d9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
